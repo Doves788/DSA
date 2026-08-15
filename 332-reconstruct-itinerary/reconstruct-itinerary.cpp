6269d94c57c5a2281f1013171fe70371d2c86125
Devesh Mehra <mhrdevesh@gmail.com>
2026-08-16 01:04:09 +0530

    Time: 11 ms (26.27%) | Memory: 22.1 MB (12.25%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/reconstruct-itinerary.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    void run_dfs(string cur_air,
-                 map<string, vector<string>>& flt_map,
-                 vector<string>& ans_arr) {
-        while (!flt_map[cur_air].empty()) {
-            string nxt_air = flt_map[cur_air].back();
-            flt_map[cur_air].pop_back();
-
-            run_dfs(nxt_air, flt_map, ans_arr);
+    map<string, multiset<string>> graph;
+    vector<string> res;
+    void dfs(string vertex) 
+    {
+        while(!graph[vertex].empty())
+        {
+            string to_vertex = *(graph[vertex].begin());
+            graph[vertex].erase(graph[vertex].begin());
+            dfs(to_vertex);
         }
-        ans_arr.push_back(cur_air);
+        res.push_back(vertex);
     }
+
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        map<string, vector<string>> flt_map;
-        int num_tkt = tickets.size();
-        for (int idx = 0; idx < num_tkt; idx++) {
-            string dep_air = tickets[idx][0];
-            string arr_air = tickets[idx][1];
-            flt_map[dep_air].push_back(arr_air);
+        for (auto ticket : tickets) {
+            graph[ticket[0]].insert(ticket[1]);
         }
-        for (auto& pair_it : flt_map) {
-            sort(pair_it.second.begin(),
-                 pair_it.second.end(),
-                 greater<string>());
-        }
-        vector<string> ans_arr;
-        run_dfs("JFK", flt_map, ans_arr);
-        reverse(ans_arr.begin(), ans_arr.end());
-        return ans_arr;
+        dfs("JFK");
+        reverse(res.begin(), res.end());
+        return res;
     }
 };
